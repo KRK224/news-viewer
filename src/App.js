6e3eeb5c250af340a3a555dc22f8b1031f8d9463
KRyun <hihas93@naver.com>
2021-07-24 20:28:07 +0900

    News-App Router까지

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import axios from 'axios';
+import NewsList from './Components/NewsList'
+import Categories from './Components/Categories';
+import NewsPage from './pages/NewsPage';
+import {Route} from 'react-router-dom';
 
 const App = () => {
-  const [data, setData] = useState(null);
 
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=80a707009cf04ffe956bd51f9032593e'
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  /*  useState로 만든 Category 기능
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category =>
+    setCategory(category), []);
+  */
 
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
+    /*
+      상태관리로 만든 News Category
+    <>
+    
+      <Categories category={category} onSelect={onSelect}/>
+      <NewsList category={category} />
+    </>
+
+    */
+
+    <Route path="/:category?" component={NewsPage} />
+    
+    
+
   );
 };
 
